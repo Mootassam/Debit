d918a9862c5e0dffeb78465661606e1471280b9c
unknown <goafucheng@gmail.com>
2023-11-28 14:09:54 +0400

    update the codes and the header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,20 +1,7 @@
-
-import { useSelector } from "react-redux";
-import {  selectDebit } from "./../store/debit/debitSelectors";
-import { useDispatch } from "react-redux/es/exports";
-
-import { ThunkDispatch } from "@reduxjs/toolkit";
-
-
 function Header() {
-  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
-  const debitData = useSelector(selectDebit);
-
   // const clearAll = async () => {
   //   await dispatch(deleteDebit());
   // };
-
-
 
   return (
     <>
@@ -28,14 +15,12 @@ function Header() {
         <HiCreditCard color="white" size={25} />
         <span className="number__credit">3</span>
       </div> */}
-        <label></label>
-{/* 
+      
+        {/* 
         <label className="clear__all" onClick={() => clearAll()}>
           Clear All : <span className="credit__number">{debitData.length}</span>
         </label> */}
       </div>
-
-    
     </>
   );
 }
